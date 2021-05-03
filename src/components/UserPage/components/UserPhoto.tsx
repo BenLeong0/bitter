35317dde9c40886182fe15fbdfdfc0ed1f7e9c15
= <benleong0@gmail.com>
2021-05-03 21:14:44 +0100

    userpage pfp

--- src/components/UserPage/components/UserPhoto.tsx
+++ src/components/UserPage/components/UserPhoto.tsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const UserPhoto: React.FC<{ handle: string }> = ({ handle }) => {
+  const [src, setSrc] = useState<string>("");
+
+  const onError = () => {
+    console.log("errorr");
+
+    setSrc(`${process.env.PUBLIC_URL}/placeholderpfp.png`);
+  };
+
+  useEffect(() => {
+    setSrc("https://bitter-imgs.s3.eu-west-2.amazonaws.com/pfp-" + handle);
+  }, [handle]);
+
   return (
     <div className="user-photo">
-      <img
-        src={`${process.env.PUBLIC_URL}/placeholderpfp.png`}
-        alt="profile pic"
-      />
+      <img src={src} onError={onError} alt="profile pic" />
     </div>
   );
 };
