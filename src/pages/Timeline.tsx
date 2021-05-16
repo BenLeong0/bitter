7ed9d5483b634f77afe6626f15135f7dfdab3fd9
= <benleong0@gmail.com>
2021-05-16 23:22:56 +0100

    Variable tab title

--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -4,7 +4,8 @@ import TimelineBitList from "../components/Timeline/TimelineBitList";
 import "../components/Timeline/Timeline.css";
 
 const Timeline: React.FC<{ timelineType: string }> = ({ timelineType }) => {
-  document.title = "Home / Bitter";
+  const pageTitle = timelineType === "all" ? "All posts" : "Home";
+  document.title = `${pageTitle} / Bitter`;
 
   return (
     <div className="timeline">
