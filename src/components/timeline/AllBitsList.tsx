f3adaac90164d8bbb30db1370214e5b851596f80
= <benleong0@gmail.com>
2021-05-16 17:57:02 +0100

    use bits/all api endpoint

--- src/components/timeline/AllBitsList.tsx
+++ src/components/timeline/AllBitsList.tsx
@@ -26,7 +26,8 @@ const AllBitsList: React.FC<{}> = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [requestCounter, setRequestCounter] = useState<number>(0);
 
-  const { API_URL, refreshList }: ContextProps = useContext(AccountContext);
+  const { API_URL, refreshList, myHandle }: ContextProps =
+    useContext(AccountContext);
 
   useEffect(() => {
     fetchBits();
@@ -41,7 +42,7 @@ const AllBitsList: React.FC<{}> = () => {
     // setBits([]);
     console.log("Fetching bits...");
 
-    const data = await fetch(`${API_URL}/bits/timeline`);
+    const data = await fetch(`${API_URL}/bits/all?handle=${myHandle}`);
     const resp: any = await data.json();
 
     // Only update if final request ie ignore if another request was sent out after
