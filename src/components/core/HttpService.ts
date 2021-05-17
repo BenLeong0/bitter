4aa2b02aed815d56fefffe73b1e912f01322407e
= <benleong0@gmail.com>
2021-05-17 20:10:01 +0100

    makePatchRequest

--- src/components/core/HttpService.ts
+++ src/components/core/HttpService.ts
@@ -17,6 +17,21 @@ export default class HttpService {
         return resp;
     }
 
+    async makePatchRequest(url: string, body: any): Promise<any> {
+        let {headers} = await this.coreService.getSession();
+        headers["Content-Type"] = "application/json";
+
+        let requestOptions = {
+            headers,
+            method: "PATCH",
+            body: JSON.stringify(body),
+        };
+
+        let data: any = await fetch(url, requestOptions);
+        let resp: string = await data.text();
+        return JSON.parse(resp);
+    }
+
     async uploadImage(url: string, file: any, field?: string): Promise<any> {
         const base64File = await this.coreService.toBase64(file);
         if (typeof base64File !== "string") return;
