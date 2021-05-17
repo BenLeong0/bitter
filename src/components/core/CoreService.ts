2307fbb38543ea59e9db77efd1ca8c17afd681b7
= <benleong0@gmail.com>
2021-05-17 19:58:12 +0100

    rearrange getSession

--- src/components/core/CoreService.ts
+++ src/components/core/CoreService.ts
@@ -5,51 +5,47 @@ export default class Common {
     getSession = async (): Promise<any> =>
         await new Promise((resolve, reject) => {
         const user: CognitoUser | null = Pool.getCurrentUser();
-        if (user) {
-            user.getSession(
-            async (err: Error, session: CognitoUserSession | null) => {
-                if (err) {
-                reject();
-                } else if (session) {
-                const attributes: any = await new Promise((resolve, reject) => {
-                    user.getUserAttributes(
-                    (
-                        err: Error | undefined,
-                        attributes: CognitoUserAttribute[] | undefined
-                    ) => {
-                        if (err) {
-                        reject(err);
-                        } else if (attributes) {
-                        const results: any = {};
+        if (user == null) return reject("Not logged in");
+        user.getSession(
+        async (err: Error, session: CognitoUserSession | null) => {
+            if (err) {
+            reject();
+            } else if (session) {
+            const attributes: any = await new Promise((resolve, reject) => {
+                user.getUserAttributes(
+                (
+                    err: Error | undefined,
+                    attributes: CognitoUserAttribute[] | undefined
+                ) => {
+                    if (err) {
+                    reject(err);
+                    } else if (attributes) {
+                    const results: any = {};
 
-                        for (let attribute of attributes) {
-                            const { Name, Value } = attribute;
-                            results[Name] = Value;
-                        }
-
-                        resolve(results);
-                        }
+                    for (let attribute of attributes) {
+                        const { Name, Value } = attribute;
+                        results[Name] = Value;
                     }
-                    );
-                });
 
-                const token = session.getIdToken().getJwtToken();
-
-                resolve({
-                    user,
-                    headers: {
-                    Authorization: token,
-                    "x-api-key": attributes["custom:apikey"],
-                    },
-                    ...session,
-                    ...attributes,
-                });
+                    resolve(results);
+                    }
                 }
+                );
+            });
+
+            const token = session.getIdToken().getJwtToken();
+
+            resolve({
+                user,
+                headers: {
+                Authorization: token,
+                "x-api-key": attributes["custom:apikey"],
+                },
+                ...session,
+                ...attributes,
+            });
             }
-            );
-        } else {
-            reject("Not logged in");
-        }
+        });
     });
 
     toBase64 = (file: any) =>
