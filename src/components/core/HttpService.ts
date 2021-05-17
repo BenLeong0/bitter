1451c5f219b55001b8540c24050d29336e12dbc6
= <benleong0@gmail.com>
2021-05-17 19:54:10 +0100

    uploadImage method

--- src/components/core/HttpService.ts
+++ src/components/core/HttpService.ts
@@ -16,4 +16,29 @@ export default class HttpService {
         let resp: any = await data.json();
         return resp;
     }
+
+    async uploadImage(url: string, file: any, field?: string): Promise<any> {
+        const base64File = await this.coreService.toBase64(file);
+        if (typeof base64File !== "string") return;
+
+        let {headers} = await this.coreService.getSession();
+        headers["Content-Type"] = "application/json";
+        console.log(headers);
+
+        let fileType = file.type;
+
+        let requestOptions = {
+            headers,
+            method: "PUT",
+            body: JSON.stringify({
+                image: base64File,
+                field,
+                type: fileType,
+            }),
+        };
+
+        let data: any = await fetch(url, requestOptions);
+        let resp: string = await data.text();
+        return JSON.parse(resp);
+    }
 }
