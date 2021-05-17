2add63ad941e391d5b27e9999ebb098e4d35d7b9
= <benleong0@gmail.com>
2021-05-17 19:54:00 +0100

    import coreservice

--- src/components/core/HttpService.ts
+++ src/components/core/HttpService.ts
@@ -1,10 +1,19 @@
+import CoreService from "./CoreService"
+
+
 export default class HttpService {
+    coreService: CoreService
+
+    constructor() {
+        this.coreService = new CoreService()
+    }
+
     async makeGetRequest(url: string): Promise<any> {
         const requestOptions = {
             method: "GET"
-        }
-        const data = await fetch(url, requestOptions);
-        const resp: any = await data.json();
-        return resp
+        };
+        let data: any = await fetch(url, requestOptions);
+        let resp: any = await data.json();
+        return resp;
     }
 }
