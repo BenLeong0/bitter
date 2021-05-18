aa37d79969e9fead9b6893ddb790b4baded76039
= <benleong0@gmail.com>
2021-05-18 19:59:27 +0100

    http requests use res

--- src/components/core/HttpService.ts
+++ src/components/core/HttpService.ts
@@ -3,24 +3,30 @@ import CoreService from "./CoreService";
 
 export default class HttpService {
     coreService: CoreService;
+    url: string = "https://7z39hjjfg1.execute-api.eu-west-2.amazonaws.com";
+    stage: string = "/dev";
+    API_URL: string = this.url + this.stage;
 
     constructor() {
         this.coreService = new CoreService();
     }
 
-    async makeGetRequest(url: string): Promise<any> {
+    async makeGetRequest(res: string): Promise<any> {
+        let url = this.url + res;
         let requestOptions = {
             method: "GET"
         };
+
         let data: any = await fetch(url, requestOptions);
         let resp: any = await data.json();
         return resp;
     }
 
-    async makePostRequest(url: string, body: any): Promise<any> {
+    async makePostRequest(res: string, body: any): Promise<any> {
         let {headers} = await this.coreService.getSession();
         headers["Content-Type"] = "application/json";
 
+        let url = this.url + res;
         let requestOptions = {
             headers,
             method: "POST",
@@ -32,10 +38,11 @@ export default class HttpService {
         return JSON.parse(resp);
     }
 
-    async makeDeleteRequest(url: string, body: any): Promise<any> {
+    async makeDeleteRequest(res: string, body: any): Promise<any> {
         let {headers} = await this.coreService.getSession();
         headers["Content-Type"] = "application/json";
 
+        let url = this.url + res;
         let requestOptions = {
             headers,
             method: "DELETE",
@@ -47,10 +54,11 @@ export default class HttpService {
         return JSON.parse(resp);
     }
 
-    async makePatchRequest(url: string, body: any): Promise<any> {
+    async makePatchRequest(res: string, body: any): Promise<any> {
         let {headers} = await this.coreService.getSession();
         headers["Content-Type"] = "application/json";
 
+        let url = this.url + res;
         let requestOptions = {
             headers,
             method: "PATCH",
@@ -62,7 +70,7 @@ export default class HttpService {
         return JSON.parse(resp);
     }
 
-    async uploadImage(url: string, file: any, field?: string): Promise<any> {
+    async uploadImage(res: string, file: any, field?: string): Promise<any> {
         let base64File = await this.coreService.toBase64(file);
         if (typeof base64File !== "string") return;
         let fileType = file.type;
@@ -70,6 +78,7 @@ export default class HttpService {
         let {headers} = await this.coreService.getSession();
         headers["Content-Type"] = "application/json";
 
+        let url = this.url + res;
         let requestOptions = {
             headers,
             method: "PUT",
