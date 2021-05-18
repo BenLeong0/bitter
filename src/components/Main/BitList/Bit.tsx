5a840fb32427ada83060df4283e488aaa51a3e33
= <benleong0@gmail.com>
2021-05-18 21:42:17 +0100

    delete bit

--- src/components/Main/BitList/Bit.tsx
+++ src/components/Main/BitList/Bit.tsx
@@ -135,18 +135,16 @@ const Bit: React.FC<BitProps> = ({ classes = "", ...bitInfo }) => {
   const handleDeletePost = async (e: any) => {
     e.preventDefault();
 
-    let url = `${API_URL}/bits`;
+    let res = `/bits`;
     let body = { post_id: bitInfo.post_id };
-    await httpService
-      .makeDeleteRequest(url, body)
-      .then((resp) => {
-        if (resp.code === "deleteSuccess") {
-          setIsDeleted(true);
-        } else {
-          console.error(resp);
-        }
-      })
-      .catch((error) => console.error("Error:", error));
+    let resp: any = await httpService.makeDeleteRequest(res, body);
+
+    if (resp.code === "deleteSuccess") {
+      setIsDeleted(true);
+      console.log(resp);
+    } else {
+      console.error(resp);
+    }
   };
 
   // Delete popover
