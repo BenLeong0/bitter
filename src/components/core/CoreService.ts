2aeebfa49bc445c206579a72bf4f1c47c6dda2a7
= <benleong0@gmail.com>
2021-05-18 22:17:25 +0100

    authenticate function in coreservice

--- src/components/core/CoreService.ts
+++ src/components/core/CoreService.ts
@@ -1,4 +1,4 @@
-import { CognitoUser, CognitoUserAttribute, CognitoUserSession } from "amazon-cognito-identity-js";
+import { AuthenticationDetails, CognitoUser, CognitoUserAttribute, CognitoUserSession } from "amazon-cognito-identity-js";
 import Pool from "../../UserPool";
 
 export default class Common {
@@ -41,6 +41,32 @@ export default class Common {
             });
         });
     });
+
+
+    authenticate = async (Password: string) => {
+        const Username = await this.getSession().then(({user}) => user.username);
+        const user = new CognitoUser({Username, Pool })
+        const authDetails = new AuthenticationDetails({ Username, Password });
+
+        return await new Promise((resolve, reject) => {
+            user.authenticateUser(authDetails, {
+                onSuccess: (data) => {
+                  console.log("onSuccess:", data);
+                  resolve(data);
+                },
+
+                onFailure: (err) => {
+                  console.error("onFailure:", err);
+                  reject(err);
+                },
+
+                newPasswordRequired: (data) => {
+                  console.log("newPasswordRequired:", data);
+                  resolve(data);
+                },
+            });
+        });
+    }
 
 
     toBase64 = (file: any) =>
