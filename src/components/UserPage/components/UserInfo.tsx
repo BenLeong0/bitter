e07ecba315ee14f5d0416bc7bcb37a5e6c6320b0
= <benleong0@gmail.com>
2021-05-20 20:06:52 +0100

    neatened userinfo

--- src/components/UserPage/components/UserInfo.tsx
+++ src/components/UserPage/components/UserInfo.tsx
@@ -32,39 +32,33 @@ const UserInfo: React.FC<Props> = ({ updatePageState, user }) => {
       <div className="user-bio">{user.bio}</div>
       <div className="user-tabs">
         <div
-          className={
-            "user-tab-bits" + (listId === 0 ? " user-current-tab" : "")
-          }
+          className={`user-tab-bits${listId === 0 ? " user-current-tab" : ""}`}
           onClick={() => changeTab(0)}
         >
           <b>{user.bits_count}</b> Bits
         </div>
         <div
-          className={
-            "user-tab-replies" + (listId === 1 ? " user-current-tab" : "")
-          }
+          className={`user-tab-replies${
+            listId === 1 ? " user-current-tab" : ""
+          }`}
           onClick={() => changeTab(1)}
         >
           <b>{user.replies}</b> Bits and replies
         </div>
         <div
-          className={
-            "user-following" + (listId === 2 ? " user-current-tab" : "")
-          }
+          className={`user-following${listId === 2 ? " user-current-tab" : ""}`}
           onClick={() => changeTab(2)}
         >
           <b>{user.following_count}</b> Following
         </div>
         <div
-          className={
-            "user-followers" + (listId === 3 ? " user-current-tab" : "")
-          }
+          className={`user-followers${listId === 3 ? " user-current-tab" : ""}`}
           onClick={() => changeTab(3)}
         >
           <b>{user.follower_count}</b> Followers
         </div>
         <div
-          className={"user-likes" + (listId === 4 ? " user-current-tab" : "")}
+          className={`user-likes${listId === 4 ? " user-current-tab" : ""}`}
           onClick={() => changeTab(4)}
         >
           <b>{user.likes}</b> Likes
